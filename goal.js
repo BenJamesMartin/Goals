1a2c52541c060aa7aadb7c033610911b2947f6d5
Ben Martin <tennisracket22@yahoo.com>
2013-12-09 22:38:27 -0800

    Properly appended DOM elements

--- goal.js
+++ goal.js
@@ -1,10 +1,14 @@
 (function(){
 
+	// Returns a goal 
 	var Goal = function(title, percentageComplete) {
 
 		// Make div and append to page with proper width based on percentage
 		// Make an h1 with goal name
 		// Make green become more intense as goal becomes completed
+
+		var goalWrapper = document.createElement('div');
+
 
 		var goalTitle = document.createElement('h1');
 		goalTitle.setAttribute('class', 'goal-title');
@@ -14,17 +18,26 @@
 		goalContainer.setAttribute('class', 'goal-container');
 
 		var progressContainer = document.createElement('div');
-		progressContainer.setAttribute('class', 'progress-container');
+		progressContainer.setAttribute('class', 'goal-progress');
 		progressContainer.style.width = 300*percentageComplete + 'px';
 
+		goalWrapper.appendChild(goalTitle);
+		goalWrapper.appendChild(goalContainer);
+		goalContainer.appendChild(progressContainer);
+
+		return goalWrapper;
 
 	};
+
+	var goal = new Goal('Work at Medium', 0.7);
+
+	document.body.appendChild(goal);
 
 
 
 	window.onload = function(){
 
-		enableDrag();
+		// enableDrag();
 
 	};
 
